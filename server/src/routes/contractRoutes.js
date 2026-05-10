87537fcf53cc8130afb3dd4945419d73e4c8139c
DoDangNhatTienK23 <ddntien23@clc.fitus.edu.vn>
2026-05-10 18:21:48 +0700

    Fix ghi nhận cư trú

--- server/src/routes/contractRoutes.js
+++ server/src/routes/contractRoutes.js
@@ -2,12 +2,12 @@ const express = require("express");
 const router = express.Router();
 const contractRecordController = require("../controllers/contractRecordController");
 
-const { verifyToken } = require("../middlewares/authMiddleware");
+const { authMiddleware } = require("../middlewares/authMiddleware");
 
 router.get('/rooms', contractRecordController.getRooms);
 router.get('/active', contractRecordController.getActiveContracts);
 router.get('/customer', contractRecordController.findCustomer);
 router.get('/:idHopDong', contractRecordController.getContractById);
-router.post('/residence', verifyToken, contractRecordController.recordResidence);
+router.post('/residence', authMiddleware, contractRecordController.recordResidence);
 
 module.exports = router;
